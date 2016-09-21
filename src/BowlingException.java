057dc14490be546f1bf1b636f6a37f153be7f451
somename <someemail>
2016-09-21 14:49:07 +0300

    besouro automatic message

--- src/BowlingException.java
+++ src/BowlingException.java
@@ -1,4 +1,12 @@
 
 public class BowlingException extends Exception {
+	private String msg;
+
+	public BowlingException(String string) {
+		this.msg = string;
+	}
+	public String getMessage(){
+		return msg;
+	}
 
 }
