6c9c563d41095e41b556c8717485393793dd82bb
somename <someemail>
2016-09-21 15:47:23 +0300

    besouro automatic message

--- src/BonusFrame.java
+++ src/BonusFrame.java
@@ -2,7 +2,7 @@
 public class BonusFrame extends Frame {
 
 	public BonusFrame(int firstThrow, int secondThrow) throws BowlingException {
-		super(firstThrow, secondThrow);
+		super(firstThrow, secondThrow, true);
 	}
 	
 
